5d49782d0bc8638664910b8e5d2319aec2706b19
Luca Coraci <luca.coraci@gmail.com>
2022-06-04 01:56:16 +0200

    init

---
+++ src/main/java/it/cnr/istc/actions/Calculator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package it.cnr.istc.actions;
+
+/**
+ *
+ * @author sommovir
+ */
+public class Calculator {
+    
+    public static int sum(int a , int b){
+        return a+b;
+    }
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+
+import it.cnr.istc.actions.Calculator;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author sommovir
+ */
+public class CalculatorTest {
+    
+    public CalculatorTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+    
+    
+    @Test
+    public void testSum(){
+        assertEquals(4,Calculator.sum(1, 3),"errore mi aspettavo 4");
+        System.out.println("[TEST SUM] OK");
+    }
+    
+}
